e8f713e2a5d07a70252bbce1fff05ae64c4c2cd3
Bismarck-C <90526284+Bismarck-C@users.noreply.github.com>
2022-09-21 19:22:12 -0600

    carpetas creadas

---
+++ proyecto3_progra3/src/main/java/com/una/proyecto3_progra3/main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.una.proyecto3_progra3;
+
+/**
+ *
+ * @author villa
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
